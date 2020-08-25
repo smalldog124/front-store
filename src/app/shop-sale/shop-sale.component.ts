90f9e78c2a4fd7e60b16e4ffad03ab03a0b04ff2
smalldog124 <panumars124@gmail.com>
2020-08-25 19:40:18 +0700

    edit: แก้ไขให้ถึงข้อมูลจาก API ที่หน้า shop sale

--- src/app/shop-sale/shop-sale.component.ts
+++ src/app/shop-sale/shop-sale.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Sort } from '@angular/material/sort';
+import { BasketService } from '../basket.service';
 
-export interface Dessert {
-  calories: number;
-  carbs: number;
-  fat: number;
+export interface Product {
+  quantity: number;
   name: string;
-  protein: number;
+  unit_price: number;
+  amount: number;
 }
 
 @Component({
@@ -15,45 +14,19 @@ export interface Dessert {
   styleUrls: ['./shop-sale.component.css']
 })
 export class ShopSaleComponent implements OnInit {
-  
-  desserts: Dessert[] = [
-    {name: 'Frozen yogurt', calories: 159, fat: 6, carbs: 24, protein: 4},
-    {name: 'Ice cream sandwich', calories: 237, fat: 9, carbs: 37, protein: 4},
-    {name: 'Eclair', calories: 262, fat: 16, carbs: 24, protein: 6},
-    {name: 'Cupcake', calories: 305, fat: 4, carbs: 67, protein: 4},
-    {name: 'Gingerbread', calories: 356, fat: 16, carbs: 49, protein: 4},
-  ];
+  products: Product[];
+  total_price: number;
 
-  sortedData: Dessert[];
-
-  constructor() { 
-    this.sortedData = this.desserts.slice();
-  }
+  constructor(private basketService: BasketService) { }
 
   ngOnInit(): void {
   }
 
-  sortData(sort: Sort) {
-    const data = this.desserts.slice();
-    if (!sort.active || sort.direction === '') {
-      this.sortedData = data;
-      return;
-    }
-
-    this.sortedData = data.sort((a, b) => {
-      const isAsc = sort.direction === 'asc';
-      switch (sort.active) {
-        case 'name': return compare(a.name, b.name, isAsc);
-        case 'calories': return compare(a.calories, b.calories, isAsc);
-        case 'fat': return compare(a.fat, b.fat, isAsc);
-        case 'carbs': return compare(a.carbs, b.carbs, isAsc);
-        case 'protein': return compare(a.protein, b.protein, isAsc);
-        default: return 0;
-      }
-    });
+  addToBasket(barcode: string,quantity_box: string) {
+    console.log(barcode,quantity_box)
+    this.basketService.addToBasket().subscribe((data: any) => {
+      this.products = data.product;
+      this.total_price = data.total_price;
+    })
   }
-}
-
-function compare(a: number | string, b: number | string, isAsc: boolean) {
-  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
