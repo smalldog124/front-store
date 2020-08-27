78a775632f479ad1463aaa8296e13ae4e77319a9
smalldog124 <panumars124@gmail.com>
2020-08-27 15:49:37 +0700

    added: เพิ่ม route หน้าเพิ่มสินค้า

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ShopSaleComponent } from './shop-sale/shop-sale.component'
 import { WholesaleComponent } from './wholesale/wholesale.component'
+import { AddProductComponent } from './add-product/add-product.component'
 
 const routes: Routes = [
-{ path: 'shop-sale',component: ShopSaleComponent},
-{ path: 'wholesale',component: WholesaleComponent}
+  { path: 'shop-sale', component: ShopSaleComponent },
+  { path: 'wholesale', component: WholesaleComponent },
+  { path: 'add-product', component: AddProductComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,18 +7,24 @@ import { AppComponent } from './app.component';
 import { TypeSellingComponent } from './type-selling/type-selling.component';
 import { ShopSaleComponent } from './shop-sale/shop-sale.component';
 import { MatSortModule } from '@angular/material/sort';
-import {MatIconModule} from '@angular/material/icon';
-import { WholesaleComponent } from './wholesale/wholesale.component'; 
-import {MatGridListModule} from '@angular/material/grid-list'; 
-import {MatRadioModule} from '@angular/material/radio'; 
-import {MatButtonToggleModule} from '@angular/material/button-toggle'; 
+import { MatIconModule } from '@angular/material/icon';
+import { WholesaleComponent } from './wholesale/wholesale.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatTableModule } from '@angular/material/table';
+import { SlipTemplateComponent } from './slip-template/slip-template.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AddProductComponent } from './add-product/add-product.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const material = [
   MatSortModule,
   MatIconModule,
   MatGridListModule,
   MatRadioModule,
-  MatButtonToggleModule
+  MatButtonToggleModule,
+  MatTableModule
 ];
 
 @NgModule({
@@ -26,15 +33,21 @@ const material = [
     TypeSellingComponent,
     ShopSaleComponent,
     WholesaleComponent,
-    
+    SlipTemplateComponent,
+    AddProductComponent,
+
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     material
   ],
   providers: [],
-  exports:[
+  exports: [
     material
   ],
   bootstrap: [AppComponent]
